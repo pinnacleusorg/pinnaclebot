a61a8c59ba68dde1fa665b83b85ae2c4b279ff90
Kendall Goto <k@kgo.to>
2020-05-31 19:19:28 -0700

    add watch delay so we don't interrupt the git update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'Pinnaclebot',
     script: 'index.js',
     watch: '.',
+    watch_delay: 5000,
     error_file: './log/err.log',
     out_file: './log/out.log',
     ignore_watch: ['log']
