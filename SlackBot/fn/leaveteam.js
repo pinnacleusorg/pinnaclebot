91fccea4e6c9e438d67afca5189f497b9ecf62d1
Kendall Goto <k@kgo.to>
2020-06-15 20:55:43 -0700

    destroy team on leaveteam if you're the only one

--- SlackBot/fn/leaveteam.js
+++ SlackBot/fn/leaveteam.js
@@ -6,13 +6,15 @@ module.exports = function(body, ...param) {
 	var thisUser = process.globals.userInfo[body.user_id];
 	if(thisUser.team) {
 		var team = process.globals.teamChannels[thisUser.team];
-		if(team.leader != body.user_id) {
+		if(team.leader != body.user_id || team.members.length == 1) {
 			//ok, leave team ...
+			if(team.members.length > 1) {
+				slackref.callMethod('chat.postMessage', {channel: channelID, text: "<@"+body.user_id+"> left the team."});
+				slackref.callMethod('conversations.kick', {channel: channelID, user: body.user_id});
+			}
 			var channelID = thisUser.team;
 			thisUser.team = false;
 			team.members.splice(team.members.indexOf(body.user_id), 1);
-			slackref.callMethod('chat.postMessage', {channel: channelID, text: "<@"+body.user_id+"> left the team."});
-			slackref.callMethod('conversations.kick', {channel: channelID, user: body.user_id});
 			return "You've successfully left your team.";
 		}
 		return "You can't leave your team since you're the owner -- delegate the role (`/p setowner @[name]`) first.";
