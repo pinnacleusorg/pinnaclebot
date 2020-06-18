38ed6acd656def17378260a6ec15ce98aea36bb9
Kendall Goto <k@kgo.to>
2020-06-17 21:50:08 -0700

    add lookup command to dump data

---
+++ SlackBot/fn/lookup.js
@@ -0,0 +1,17 @@
+module.exports = function(body, ...param) {
+	var slackref = process.globals.slackbot;
+	if(process.globals.privilegedList.includes(body.user_id)) {
+		//we are an admin using this command ... admin the target!
+		var getUser = param[0]; //parse string ...
+		if(getUser && getUser != "") {
+			var userString = getUser.split('@').pop().split('|')[0].trim();
+			if(userString != "") {
+				return "```\n"+JSON.stringify(process.globals.userInfo[userString], null, 2)+"\n```";
+			}
+			//otherwise, assume team
+			return "```\n"+JSON.stringify(process.globals.teamChannels[getUser], null, 2)+"\n```";
+		}
+		return "You have to tag a user to dump `/yeti lookup @Yeti` or `/yeti lookup [teamid]`";
+	}
+	return "Nice try, sucker!";
+};
