f6cc56e79e6a734c21f8655eb3bd6ce7358c9a0e
Kendall Goto <k@kgo.to>
2020-06-18 00:26:19 -0700

    admin add and dropin

---
+++ SlackBot/fn/add.js
@@ -0,0 +1,18 @@
+module.exports = async function(body, ...param) {
+	var slackref = process.globals.slackbot;
+	var team = process.globals.teamChannels[param[0]];
+	var thisUser = process.globals.userInfo[body.user_id];
+	if(process.globals.privilegedList.includes(body.user_id)) {
+		if(team) {
+			//invitation isn't invalid, team isn't full. we should be able to add this user.
+			var waitForInvite = new Promise(function(resolve, rej) {
+				slackref.callMethod('conversations.invite', {channel: param[0], users: body.user_id }, resolve);
+			});
+			await waitForInvite;
+			slackref.callMethod('chat.postMessage', {channel: param[0], text: "<!channel>: <@"+body.user_id+"> has joined the channel (Staff Drop-in)!" });
+
+			return "OK, I've added you into the team room - happy hacking!";
+		}
+	}
+	return "You don't seem to be checked in ... can you do `/yeti checkin` first?";
+};
---
+++ SlackBot/fn/dropin.js
@@ -0,0 +1,79 @@
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+module.exports = async function(body, ...param) {
+    if(!process.globals.privilegedList.includes(body.user_id)) {
+        return "Nice try, sucker!";
+    }
+	//get ten teams, sorted least players to most players + with title as non-null, otherwise random
+	var lfgTeams = Object.keys(process.globals.teamChannels);
+	//filter out ones with no name ...
+	var teamList = [];
+	for(var i = 0; i < lfgTeams.length; i++) {
+		var teamID = lfgTeams[i];
+		var team = process.globals.teamChannels[teamID];
+		if(team && team.title != "Untitled" && team.dropin && team.members.length > 0) {
+			teamList.push(teamID);
+		}
+	}
+	shuffleArray(teamList);
+	//present the first n elements ...
+	var result = {
+		"blocks": [
+			{
+				"type": "section",
+				"text": {
+					"type": "mrkdwn",
+					"text": ":mag: Here's a random collection of teams open for drop-in!"
+				}
+			},
+			{
+				"type": "divider"
+			}
+		]
+	}
+	if(teamList.length == 0) {
+		result.blocks.push({
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "No teams currently available."
+			}
+		});
+	}
+	for(var i = 0; i < teamList.length && i < 10; i++) {
+		var team = process.globals.teamChannels[teamList[i]];
+		var teamName = team.title;
+		var num = team.members.length;
+		var description = team.description;
+		var truncatedDesc = "";
+		if (description.length > 60)
+			truncatedDesc = description.substring(0, 60) + '...';
+		else
+			truncatedDesc = description;
+
+		result.blocks.push({
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": (i+1)+". *"+teamName+"* - "+truncatedDesc+"\nJoin: `/yeti add "+teamList[i]+"`` ("+num+"/4 members)"
+			}
+		});
+	}
+	result.blocks.push({
+			"type": "divider"
+	});
+	result.blocks.push({
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "To see more teams, just do `/yeti dropin` again."
+			}
+	});
+
+	return result;
+};
