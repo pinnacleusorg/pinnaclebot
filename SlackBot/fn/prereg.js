328271369d95f716f56ac3efcd8dca5bef2c3184
Kendall Goto <k@kgo.to>
2020-06-18 22:12:16 -0700

    always perfect

--- SlackBot/fn/prereg.js
+++ SlackBot/fn/prereg.js
@@ -5,7 +5,7 @@ module.exports = function(body, ...param) {
 		var url = getUser.split('<').pop().split('|')[0].trim().replace('>', '').replace('mailto', '');
 		if(getUser && getUser != "") {
 			//pre-reg
-			process.globals.preCheckin_list.push(getUser);
+			process.globals.preCheckin_list.push(url);
 			return "I've added "+getUser+" to the pre-register list. They should be able to checkin now.";
 		}
 		return "You need to write an email to pre-register, like `/yeti prereg kendall@pinnacle.us.org`.";
