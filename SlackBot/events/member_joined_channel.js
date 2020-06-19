d0900a51aa5b5cf10e173cd3ed0dee9c7a9a3e59
Kendall Goto <k@kgo.to>
2020-06-18 21:23:35 -0700

    why is this still being triggered

--- SlackBot/events/member_joined_channel.js
+++ SlackBot/events/member_joined_channel.js
@@ -4,6 +4,7 @@ module.exports = function(body) {
 	var thisUser = process.globals.userInfo[body.user_id];
 	var slackref = process.globals.slackbot;
 	if(!thisUser || !thisUser.team || body.event.channel != thisUser.team) {
+		console.log(thisUser);
 		slackref.callMethod('chat.postEphemeral', {channel: body.event.channel, user: body.event.user, text: "Welcome in to the channel! You will need to be officially invited to be registered onto this team. Tell the owner to `/yeti invite` you!"});
 		return "doesn't have team OR this isn't their team ..."
 	}
